9c6967cdaca0d0b7ea4ffe9b6103fbd78cf9e680
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-09 01:02:46 +0200

    feat: click sul bottone. (toggle)

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,16 +4,23 @@ import { useState } from "react";
 
 function Main() {
 
+    // useState per mostrare/nascondere degli elementi
     const [isOpen, setIsOpen] = useState(true);
+
+    // con prev => !prev, facciamo in modo che si verifichi: la prima volta una condizione, la volta dopo la condizione opposta
+    const toggle = () => {
+        setIsOpen(prev => !prev)
+    };
 
     return <main>
         <div className="container">
             <div className="button-container">
                 {languages.map(languages =>
-                    <button key={languages.id}>
+                    <button key={languages.id} onClick={toggle}>
                         {languages.title}
                     </button>)}
             </div>
+
             {isOpen && <div>
                 <ExplanationText />
             </div>}
